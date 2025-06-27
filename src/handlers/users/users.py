1055ec6bbac39c2f12f5b7fe1992a2a5c347ea61
Windows 10 <adhambekhusinboyev@gmail.com>
2025-06-28 03:13:14 +0500

    first

--- src/handlers/users/users.py
+++ src/handlers/users/users.py
@@ -25,23 +25,23 @@ async def start_cmd1(message: Message):
             "UPDATE referal SET starter = FALSE WHERE user_id = %s", (user_id,)
         )
         conn.commit()
-    #
-    # sql.execute("SELECT ready, chance FROM public.referal WHERE user_id=%s", (user_id, ))
-    # result = sql.fetchone()
-    # if result:
-    #     ready, chance = result
-    #     if ready is True:
-    #         await message.answer("Botimizga xush kelibsiz, kerakli bo'limni tanlab va davom eting!", parse_mode="html",
-    #                              reply_markup=await UserPanels.asos_manu())
-    #     elif chance and ready is False:
-    #         sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
-    #         number = sql.fetchone()
-    #         await message.answer(
-    #             f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> \n<code>https://t.me/BMB_testbot?start={user_id}</code>\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number[0]} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
-    #             parse_mode="html",
-    #             reply_markup=await CheckData.share_link(user_id))
-    #     elif chance is False:
-    #         await message.answer("Botimizga xush kelibsiz", reply_markup=await UserPanels.chance_manu())
+
+    sql.execute("SELECT ready, chance FROM public.referal WHERE user_id=%s", (user_id, ))
+    result = sql.fetchone()
+    if result:
+        ready, chance = result
+        if ready is True:
+            await message.answer("Botimizga xush kelibsiz, kerakli bo'limni tanlab va davom eting!", parse_mode="html",
+                                 reply_markup=await UserPanels.asos_manu())
+        elif chance and ready is False:
+            sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
+            number = sql.fetchone()
+            await message.answer(
+                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> \n<code>https://t.me/BMB_testbot?start={user_id}</code>\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number[0]} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
+                parse_mode="html",
+                reply_markup=await CheckData.share_link(user_id))
+        elif chance is False:
+            await message.answer("Botimizga xush kelibsiz", reply_markup=await UserPanels.chance_manu())
 
 @user_router.callback_query(F.data == "check", F.message.chat.type == ChatType.PRIVATE)
 async def check(call: CallbackQuery):
@@ -85,40 +85,36 @@ async def start_with_ref(message: Message, command: CommandObject):
             is_start = cursor.fetchone()[0]
             if is_start:
                 print(is_start)
-                # cursor.execute(
-                #     "UPDATE referal SET member = member + 1 WHERE user_id = %s", (referal_id,)
-                # )
-                # conn.commit()
-                #
-                # cursor.execute("SELECT member FROM referal WHERE user_id = %s", (referal_id,))
-                # number = cursor.fetchone()[0]
-                # if number>=3:
-                #     cursor.execute(
-                #         "UPDATE referal SET ready=TRUE WHERE user_id = %s", (referal_id,)
-                #     )
-                #     conn.commit()
-                #     cursor.execute(
-                #         "UPDATE referal SET starter=FALSE WHERE user_id = %s", (referal_id,)
-                #     )
-                #     conn.commit()
-                #     cursor.execute(
-                #         "UPDATE starter SET starter=FALSE WHERE user_id = %s", (user_id,)
-                #     )
-                #     conn.commit()
-    #
-    # sql.execute("SELECT ready, chance FROM public.referal WHERE user_id=%s", (user_id,))
-    # result = sql.fetchone()
-    # if result:
-    #     ready, chance = result
-    #     if ready is True:
-    #         await message.answer("Botimizga xush kelibsiz, kerakli bo'limni tanlab va davom eting!", parse_mode="html",
-    #                              reply_markup=await UserPanels.asos_manu())
-    #     elif chance and ready is False:
-    #         sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
-    #         number = sql.fetchone()
-    #         await message.answer(
-    #             f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> \n<code>https://t.me/BMB_testbot?start={user_id}</code>\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number[0]} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
-    #             parse_mode="html",
-    #             reply_markup=await CheckData.share_link(user_id))
-    #     elif chance is False:
-    #         await message.answer("Botimizga xush kelibsiz", reply_markup=await UserPanels.chance_manu())
+                cursor.execute(
+                    "UPDATE starter SET starter=%s WHERE user_id = %s", (False, user_id,)
+                )
+                conn.commit()
+                cursor.execute(
+                    "UPDATE referal SET member = member + 1 WHERE user_id = %s", (referal_id,)
+                )
+                conn.commit()
+
+                cursor.execute("SELECT member FROM referal WHERE user_id = %s", (referal_id,))
+                number = cursor.fetchone()[0]
+                if number>=3:
+                    cursor.execute(
+                        "UPDATE referal SET ready=TRUE WHERE user_id = %s", (referal_id,)
+                    )
+                    conn.commit()
+
+    sql.execute("SELECT ready, chance FROM public.referal WHERE user_id=%s", (user_id,))
+    result = sql.fetchone()
+    if result:
+        ready, chance = result
+        if ready is True:
+            await message.answer("Botimizga xush kelibsiz, kerakli bo'limni tanlab va davom eting!", parse_mode="html",
+                                 reply_markup=await UserPanels.asos_manu())
+        elif chance and ready is False:
+            sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
+            number = sql.fetchone()
+            await message.answer(
+                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> \n<code>https://t.me/BMB_testbot?start={user_id}</code>\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number[0]} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
+                parse_mode="html",
+                reply_markup=await CheckData.share_link(user_id))
+        elif chance is False:
+            await message.answer("Botimizga xush kelibsiz", reply_markup=await UserPanels.chance_manu())
