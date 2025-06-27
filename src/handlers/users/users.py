cbf658eff15da46a23c7dea9f2a05bca8183c5d5
Windows 10 <adhambekhusinboyev@gmail.com>
2025-06-28 01:14:53 +0500

    first

--- src/handlers/users/users.py
+++ src/handlers/users/users.py
@@ -36,7 +36,7 @@ async def start_cmd1(message: Message):
             sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
             number = sql.fetchone()
             await message.answer(
-                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> havola92939449\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number} ta odam taklif qildingiz, yana {3 - number}ta odam taklif qilishingiz kerak",
+                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> havola92939449\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
                 parse_mode="html",
                 reply_markup=await CheckData.share_link(user_id))
         elif chance is False:
@@ -104,7 +104,7 @@ async def start_with_ref(message: Message, command: CommandObject):
             sql.execute("SELECT member FROM public.referal WHERE user_id=%s", (user_id,))
             number = sql.fetchone()
             await message.answer(
-                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> havola92939449\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number} ta odam taklif qildingiz, yana {3 - number}ta odam taklif qilishingiz kerak",
+                f"<b>Siz yana test ishlamoqchi bo'lsangiz quyidagi havola oraqali 3 ta do'stingizni taklif qiling:</b> havola92939449\n\nEslatma: 3 ta do'stingizni taklif qilgandan so'ng, sizga <b>cheksiz test ishlash</b> hamda <b>har bir fanda alohida</b> test ishlash imkoniyati taqdim etiladi.\nSiz {number} ta odam taklif qildingiz, yana {3 - number[0]}ta odam taklif qilishingiz kerak",
                 parse_mode="html",
                 reply_markup=await CheckData.share_link(user_id))
         elif chance is False:
